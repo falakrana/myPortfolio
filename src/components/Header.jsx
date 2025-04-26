da098bdb58d4a16c760f74cb65b66446f4c4fe4c
falakrana <ranafalak18@gmail.com>
2025-04-26 23:06:41 +0530

    UpdatedPortfolio3.0

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import { Mail } from 'lucide-react'
 import { Button } from "@/components/ui/button"
 
+
 function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-black bg-opacity-50 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold text-white">FR</h1>
+        <h1 className="text-2xl font-bold text-white"><a href='#'>FR</a></h1>
         <Button variant="ghost" size="icon" onClick={() => window.location.href = 'mailto:ranafalak18@gmail.com'}>
           <Mail className="h-6 w-6 text-white" />
         </Button>
